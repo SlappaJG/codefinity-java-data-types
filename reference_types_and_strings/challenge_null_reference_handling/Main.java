7fec7dc6bd6dc798d72ef652820edfde6bfb8d5c
SlappaJG <>
2026-01-12 03:54:25 +0000

    Challenge: Null Reference Handling

---
+++ reference_types_and_strings/challenge_null_reference_handling/Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Main {
+    public static String checkString(String input) {
+        // Your code here
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkString(null));
+        System.out.println(checkString(""));
+        System.out.println(checkString("Hello world"));
+        System.out.println(checkString("test123"));
+    }
+}
